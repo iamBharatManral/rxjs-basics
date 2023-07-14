0fb49985a2e179da57ae6061f1c2fb5eda88d755
iamBharatManral <justbharatmanral@gmail.com>
2023-07-14 17:27:23 +0530

    feat: abort button with takeUntil in countdown

--- src/labs/countdown/countdown.js
+++ src/labs/countdown/countdown.js
@@ -1,13 +1,16 @@
-import {interval, map, take} from "rxjs";
+import {fromEvent, interval, map, take, takeUntil} from "rxjs";
 
 const countdown = document.querySelector(".countdown")
 const message = document.querySelector(".message")
 
+const abort = document.querySelector("button")
+const abort$ = fromEvent(abort, 'click')
 
 const countdown$ = interval(1000)
     .pipe(
         map(value => 9 - value),
-        take(10)
+        take(10),
+        takeUntil(abort$)
     )
 
 countdown$.subscribe(count => {
